f24b2d21fcc4ee09d0a02cfb7c29efad5f66be8b
highintothesky <highintothesky@gmail.com>
2015-12-01 17:54:58 +0100

    parsing work
    worked on parsing, generated a full list of the recipes, now for the
    real tokenising using nltk

--- parse.py
+++ parse.py
@@ -1,13 +1,9 @@
 # parsing and tokenizing recipe text. right now it only prints all the chunked recipes.
 # the AllRecipesData folder should be in the same location as the recipe_generation folder
 
-import csv
-import itertools
-import operator
 import numpy as np
 import os
-import sys
-from utils import *
+import nltk
 
 
 mypath = os.path.dirname(os.path.abspath(__file__))
@@ -20,12 +16,21 @@ for (dirpath, dirnames, filenames) in os.walk(chunked_path):
     dirlist.extend(dirnames)
     break
 
+recipe_paths = []
 for dirname in dirlist:
-	recipe_type_path = os.path.join(chunked_path, dirname)
-	print recipe_type_path
-	for (dirpath, dirnames, filenames) in os.walk(recipe_type_path):
-		print filenames
- #    	break
+    recipe_type_path = os.path.join(chunked_path, dirname)
+    for (dirpath, dirnames, filenames) in os.walk(recipe_type_path):
+        for name in filenames:
+            new_path = os.path.join(recipe_type_path, name)
+            recipe_paths.append(new_path)
+
+print type(dirnames)
+print type(new_path)
+print len(recipe_paths)
+print recipe_paths[200]
+
+# TODO: parse the actual recipes in the recipe_paths list using nltk
+
 
 # with open("../AllRecipesData/BananaMuffins-fulltext/almond-banana-chocolate-muffins.txt", 'rb') as f:
 # 	print f.read()
