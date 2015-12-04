2929d10ff86769d92db0ab64f11c0f87224598c6
jeroen-rooijmans <jhmrooijmans@gmail.com>
2015-12-04 11:30:48 +0100

    Load csv for RNN
    Loads chucked.csv into numpy array

---
+++ RNN_load.py
@@ -0,0 +1,26 @@
+import csv
+import itertools
+import operator
+import numpy as np
+import nltk
+import sys
+import os
+from datetime import datetime
+
+import matplotlib.pyplot as plt
+
+import parse
+
+mypath = os.path.dirname(os.path.abspath(__file__))
+chunked_path = os.path.join(mypath, "../AllRecipesData/chunked/")
+csv_path = os.path.join(mypath, "csv_data/chunked.csv")
+
+
+print "Reading CSV file..."
+with open(csv_path, 'r') as csv_file:
+	csv_data = [data for data in csv.reader(csv_file)]
+chunk_data = np.asarray(csv_data)
+
+for i in range(len(chunk_data)):
+	if chunk_data[i][0] == "PARG":
+		print chunk_data[i]
