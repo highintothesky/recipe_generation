3f448871fe76eb22c06bff9be361447d94e21ff0
highintothesky <highintothesky@gmail.com>
2015-12-08 14:50:27 +0100

    added theano rnn
    added a theano rnn (rnn3), with training and model storage at the end

--- rnn3.py
+++ rnn3.py
@@ -13,94 +13,82 @@ import theano as theano
 
 
 
-class rnn2:
+class rnn3:
     def __init__(self, word_dim, hidden_dim=100, bptt_truncate=4):
         # Assign instance variables
         self.word_dim = word_dim
         self.hidden_dim = hidden_dim
         self.bptt_truncate = bptt_truncate
         # Randomly initialize the network parameters
-        self.U = np.random.uniform(-np.sqrt(1./word_dim), np.sqrt(1./word_dim), (hidden_dim, word_dim))
-        self.V = np.random.uniform(-np.sqrt(1./hidden_dim), np.sqrt(1./hidden_dim), (word_dim, hidden_dim))
-        self.W = np.random.uniform(-np.sqrt(1./hidden_dim), np.sqrt(1./hidden_dim), (hidden_dim, hidden_dim))
+        U = np.random.uniform(-np.sqrt(1./word_dim), np.sqrt(1./word_dim), (hidden_dim, word_dim))
+        V = np.random.uniform(-np.sqrt(1./hidden_dim), np.sqrt(1./hidden_dim), (word_dim, hidden_dim))
+        W = np.random.uniform(-np.sqrt(1./hidden_dim), np.sqrt(1./hidden_dim), (hidden_dim, hidden_dim))
+        # Theano: Created shared variables
+        self.U = theano.shared(name='U', value=U.astype(theano.config.floatX))
+        self.V = theano.shared(name='V', value=V.astype(theano.config.floatX))
+        self.W = theano.shared(name='W', value=W.astype(theano.config.floatX))      
+        # We store the Theano graph here
+        self.theano = {}
+        self.__theano_build__()
 
-	def forward_propagation(self, x):
-	    # The total number of time steps
-	    T = len(x)
-	    # During forward propagation we save all hidden states in s because need them later.
-	    # We add one additional element for the initial hidden, which we set to 0
-	    s = np.zeros((T + 1, self.hidden_dim))
-	    s[-1] = np.zeros(self.hidden_dim)
-	    # The outputs at each time step. Again, we save them for later.
-	    o = np.zeros((T, self.word_dim))
-	    # For each time step...
-	    for t in np.arange(T):
-	        # Note that we are indxing U by x[t]. This is the same as multiplying U with a one-hot vector.
-	        s[t] = np.tanh(self.U[:,x[t]] + self.W.dot(s[t-1]))
-	        # print self.V.dot(s[t])
-	        o[t] = softmax(self.V.dot(s[t]))
-	    return [o, s]
-	 
-	rnn2.forward_propagation = forward_propagation
+    def __theano_build__(self):
+        U, V, W = self.U, self.V, self.W
+        x = T.ivector('x')
+        y = T.ivector('y')
+        def forward_prop_step(x_t, s_t_prev, U, V, W):
+            s_t = T.tanh(U[:,x_t] + W.dot(s_t_prev))
+            o_t = T.nnet.softmax(V.dot(s_t))
+            return [o_t[0], s_t]
+        [o,s], updates = theano.scan(
+            forward_prop_step,
+            sequences=x,
+            outputs_info=[None, dict(initial=T.zeros(self.hidden_dim))],
+            non_sequences=[U, V, W],
+            truncate_gradient=self.bptt_truncate,
+            strict=True)
+        
+        prediction = T.argmax(o, axis=1)
+        o_error = T.sum(T.nnet.categorical_crossentropy(o, y))
+        
+        # Gradients
+        dU = T.grad(o_error, U)
+        dV = T.grad(o_error, V)
+        dW = T.grad(o_error, W)
+        
+        # Assign functions
+        self.forward_propagation = theano.function([x], o)
+        self.predict = theano.function([x], prediction)
+        self.ce_error = theano.function([x, y], o_error)
+        self.bptt = theano.function([x, y], [dU, dV, dW])
+        
+        # SGD
+        learning_rate = T.scalar('learning_rate')
+        self.sgd_step = theano.function([x,y,learning_rate], [], 
+                      updates=[(self.U, self.U - learning_rate * dU),
+                              (self.V, self.V - learning_rate * dV),
+                              (self.W, self.W - learning_rate * dW)])
+    
+    def calculate_total_loss(self, X, Y):
+        return np.sum([self.ce_error(x,y) for x,y in zip(X,Y)])
+    
+    def calculate_loss(self, X, Y):
+        # Divide calculate_loss by the number of words
+        num_words = np.sum([len(y) for y in Y])
+        return self.calculate_total_loss(X,Y)/float(num_words)   
 
-	def predict(self, x):
-	    # Perform forward propagation and return index of the highest score
-	    o, s = self.forward_propagation(x)
-	    return np.argmax(o, axis=1)
 
-	rnn2.predict = predict
-
-	def bptt(self, x, y):
-	    T_bptt = len(y)
-	    # Perform forward propagation
-	    o, s = self.forward_propagation(x)
-	    # We accumulate the gradients in these variables
-	    dLdU = np.zeros(self.U.shape)
-	    dLdV = np.zeros(self.V.shape)
-	    dLdW = np.zeros(self.W.shape)
-	    delta_o = o
-	    delta_o[np.arange(len(y)), y] -= 1.
-	    # For each output backwards...
-	    for t in np.arange(T_bptt)[::-1]:
-	        dLdV += np.outer(delta_o[t], s[t].T)
-	        # Initial delta calculation
-	        delta_t = self.V.T.dot(delta_o[t]) * (1 - (s[t] ** 2))
-	        # Backpropagation through time (for at most self.bptt_truncate steps)
-	        for bptt_step in np.arange(max(0, t-self.bptt_truncate), t+1)[::-1]:
-	            # print "Backpropagation step t=%d bptt step=%d " % (t, bptt_step)
-	            dLdW += np.outer(delta_t, s[bptt_step-1])              
-	            dLdU[:,x[bptt_step]] += delta_t
-	            # Update delta for next step
-	            delta_t = self.W.T.dot(delta_t) * (1 - s[bptt_step-1] ** 2)
-	    return [dLdU, dLdV, dLdW]
-	 
-	rnn2.bptt = bptt
-
-def calculate_total_loss(self, x, y):
-    L = 0
-    # For each sentence...
-    for i in np.arange(len(y)):
-        o, s = self.forward_propagation(x[i])
-        # We only care about our prediction of the "correct" words
-        correct_word_predictions = o[np.arange(len(y[i])), y[i]]
-        # Add to the loss based on how off we were
-        L += -1 * np.sum(np.log(correct_word_predictions))
-    return L
- 
-def calculate_loss(self, x, y):
-    # Divide the total loss by the number of training examples
-    N = np.sum((len(y_i) for y_i in y))
-    return self.calculate_total_loss(x,y)/N
-
-def gradient_check(self, x, y, h=0.001, error_threshold=0.01):
-    # Calculate the gradients using backpropagation. We want to checker if these are correct.
+def gradient_check_theano(model, x, y, h=0.001, error_threshold=0.01):
+    # Overwrite the bptt attribute. We need to backpropagate all the way to get the correct gradient
+    model.bptt_truncate = 1000
+    # Calculate the gradients using backprop
     bptt_gradients = model.bptt(x, y)
-    # List of all parameters we want to check.
+    # List of all parameters we want to chec.
     model_parameters = ['U', 'V', 'W']
     # Gradient check for each parameter
     for pidx, pname in enumerate(model_parameters):
         # Get the actual parameter value from the mode, e.g. model.W
-        parameter = operator.attrgetter(pname)(self)
+        parameter_T = operator.attrgetter(pname)(model)
+        parameter = parameter_T.get_value()
         print "Performing gradient check for parameter %s with size %d." % (pname, np.prod(parameter.shape))
         # Iterate over each element of the parameter matrix, e.g. (0,0), (0,1), ...
         it = np.nditer(parameter, flags=['multi_index'], op_flags=['readwrite'])
@@ -110,12 +98,14 @@ def gradient_check(self, x, y, h=0.001, error_threshold=0.01):
             original_value = parameter[ix]
             # Estimate the gradient using (f(x+h) - f(x-h))/(2*h)
             parameter[ix] = original_value + h
+            parameter_T.set_value(parameter)
             gradplus = model.calculate_total_loss([x],[y])
             parameter[ix] = original_value - h
+            parameter_T.set_value(parameter)
             gradminus = model.calculate_total_loss([x],[y])
             estimated_gradient = (gradplus - gradminus)/(2*h)
-            # Reset parameter to original value
             parameter[ix] = original_value
+            parameter_T.set_value(parameter)
             # The gradient for this parameter calculated using backpropagation
             backprop_gradient = bptt_gradients[pidx][ix]
             # calculate The relative error: (|x - y|/(|x| + |y|))
@@ -128,22 +118,11 @@ def gradient_check(self, x, y, h=0.001, error_threshold=0.01):
                 print "Estimated_gradient: %f" % estimated_gradient
                 print "Backpropagation gradient: %f" % backprop_gradient
                 print "Relative Error: %f" % relative_error
-                return
+                return 
             it.iternext()
         print "Gradient check for parameter %s passed." % (pname)
- 
-rnn2.gradient_check = gradient_check
- 
-# Performs one step of SGD.
-def numpy_sdg_step(self, x, y, learning_rate):
-    # Calculate the gradients
-    dLdU, dLdV, dLdW = self.bptt(x, y)
-    # Change parameters according to gradients and learning rate
-    self.U -= learning_rate * dLdU
-    self.V -= learning_rate * dLdV
-    self.W -= learning_rate * dLdW
- 
-rnn2.sgd_step = numpy_sdg_step
+
+
 # Outer SGD Loop
 # - model: The RNN model instance
 # - X_train: The training data set
@@ -259,52 +238,19 @@ print "\nExample sentence after Pre-processing: '%s'" % tokenized_sentences[0]
 X_train = np.asarray([[word_to_index[w] for w in sent[:-1]] for sent in tokenized_sentences])
 y_train = np.asarray([[word_to_index[w] for w in sent[1:]] for sent in tokenized_sentences])
 
+print len(X_train)
 
 
-print "created train set"
-
-
-np.random.seed(10)
-model = rnn2(vocabulary_size)
-o, s = model.forward_propagation(X_train[10])
-# print o.shape
-# print o
-
-predictions = model.predict(X_train[10])
-# print predictions.shape
-# print predictions
-
-print "predicted some"
-
-
- 
-rnn2.calculate_total_loss = calculate_total_loss
-rnn2.calculate_loss = calculate_loss
-
-print "calculatedd loss"
-
-# Limit to 1000 examples to save time
-# print "Expected Loss for random predictions: %f" % np.log(vocabulary_size)
-# print "Actual loss: %f" % model.calculate_loss(X_train[:1000], y_train[:1000])
-
-
-# To avoid performing millions of expensive calculations we use a smaller vocabulary size for checking.
-grad_check_vocab_size = 100
-np.random.seed(10)
-model = rnn2(grad_check_vocab_size, 10, bptt_truncate=1000)
-model.gradient_check([0,1,2,3], [1,2,3,4])
-
-print "All gradient checks done"
+print "Created train set"
 
 np.random.seed(10)
-model = rnn2(vocabulary_size)
-
+model = rnn3(vocabulary_size)
 start = time.time()
 model.sgd_step(X_train[10], y_train[10], 0.005)
 end = time.time()
 print end - start
 
-np.random.seed(10)
-# Train on a small subset of the data to see what happens
-model = rnn2(vocabulary_size)
-losses = train_with_sgd(model, X_train[:100], y_train[:100], nepoch=10, evaluate_loss_after=1)
+model = rnn3(vocabulary_size)
+losses = train_with_sgd(model, X_train[:3000], y_train[:3000], nepoch=10, evaluate_loss_after=1)
+
+save_model_parameters_theano('models/trained-model-theano.npz', model)
